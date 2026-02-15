a526fe2007193cc5909ad02367ec20905c7eb219
Rachel Kiesling <80165032+opheliasmoon@users.noreply.github.com>
2026-02-14 22:47:27 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,11 @@
+from sqlalchemy import Column, String, Integer
+from database import Base
+
+class Course(Base):
+    __tablename__ = "Courses"
+
+    course_id = Column(String(10), primary_key=True)
+    course_name = Column(String(50))
+    credit_hours = Column(Integer)
+    department = Column(String(50))
 
