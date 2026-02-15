b75e0b6a776b58e85cad610d7d361a4920f3c3ba
Rachel Kiesling <80165032+opheliasmoon@users.noreply.github.com>
2026-02-14 22:47:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from fastapi import FastAPI, Depends, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+import models
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
